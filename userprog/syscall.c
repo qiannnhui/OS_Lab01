bea739ff7deb768fe1b3db9557fce2e1210759e5
qiannnhui <b09901160@ntu.edu.tw>
2025-04-07 08:45:57 +0000

    syscall_handler_v1

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -96,7 +96,7 @@ static void syscall_handler (struct intr_frame *);
 
 void syscall_init (void) 
 {
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+	intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
 
@@ -105,27 +105,166 @@ void syscall_init (void)
 */
 void sys_halt(void)
 {
-  shutdown_power_off();
+	shutdown_power_off();
 }
 
-static void syscall_handler (struct intr_frame *f UNUSED) 
-{
-  printf ("system call!\n");
-  thread_exit();
-}
 
 void
 sys_exit(struct intr_frame *f)
 {
-  printf("exit\n");
+	printf("exit\n");
 	int status;
 	pop_stack(f->esp, &status, 1);
 	exit_process(status);
 }
 
+void sys_exec(struct intr_frame *f)
+{
+	printf("exec\n");
+	// int ret;
+	// char *cmd_line;
+	// pop_stack(f->esp, &cmd_line, 2);
+	// if (!is_valid_addr(cmd_line))
+	// 	ret = -1;
+	// else
+	// {
+	// 	ret = process_execute(cmd_line);
+	// }
+	// return ret;
+}
+
+void sys_wait(struct intr_frame *f)
+{
+	printf("wait\n");
+	// int ret;
+	// int pid;
+	// pop_stack(f->esp, &pid, 3);
+	// if (pid < 0)
+	// 	ret = -1;
+	// else
+	// {
+	// 	ret = process_wait(pid);
+	// }
+	// return ret;
+}
+
+void sys_create(struct intr_frame *f)
+{
+	printf("create\n");
+	// int ret;
+	// int size;
+	// char *file_name;
+	// pop_stack(f->esp, &size, 4);
+	// pop_stack(f->esp, &file_name, 3);
+	// if (!is_valid_addr(file_name))
+	// 	ret = -1;
+	// else
+	// {
+	// 	ret = filesys_create(file_name, size);
+	// }
+	// return ret;
+}
+
+void sys_remove(struct intr_frame *f)
+{
+	printf("remove\n");
+	// int ret;
+	// char *file_name;
+	// pop_stack(f->esp, &file_name, 4);
+	// if (!is_valid_addr(file_name))
+	// 	ret = -1;
+	// else
+	// {
+	// 	ret = filesys_remove(file_name);
+	// }
+	// return ret;
+}
+
+void sys_open(struct intr_frame *f)
+{
+	printf("open\n");
+	// int ret;
+	// char *file_name;
+	// pop_stack(f->esp, &file_name, 3);
+	// if (!is_valid_addr(file_name))
+	// 	ret = -1;
+	// else
+	// {
+	// 	ret = filesys_open(file_name);
+	// }
+	// return ret;
+}
+
+void sys_filesize(struct intr_frame *f)
+{
+	printf("filesize\n");
+	// int ret;
+	// int fd;
+	// pop_stack(f->esp, &fd, 2);
+	// if (fd < 0)
+	// 	ret = -1;
+	// else
+	// {
+	// 	ret = file_length(fd);
+	// }
+	// return ret;
+}
+
+void sys_seek(struct intr_frame *f)
+{
+	printf("seek\n");
+	// int ret;
+	// int fd;
+	// int pos;
+	// pop_stack(f->esp, &pos, 4);
+	// pop_stack(f->esp, &fd, 3);
+	// if (fd < 0)
+	// 	ret = -1;
+	// else
+	// {
+	// 	ret = file_seek(fd, pos);
+	// }
+	// return ret;
+}
+
+void sys_tell(struct intr_frame *f)
+{
+	printf("tell\n");
+	// int ret;
+	// int fd;
+	// pop_stack(f->esp, &fd, 2);
+	// if (fd < 0)
+	// 	ret = -1;
+	// else
+	// {
+	// 	ret = file_tell(fd);
+	// }
+	// return ret;
+}
+
+void sys_close(struct intr_frame *f)
+{
+	printf("close\n");
+	// int ret;
+	// int fd;
+	// pop_stack(f->esp, &fd, 2);
+	// if (fd < 0)
+	// 	ret = -1;
+	// else
+	// {
+	// 	ret = file_close(fd);
+	// }
+	// return ret;
+}
+
+void sys_read(struct intr_frame *f)
+{
+	printf("read\n");
+}
+
 void sys_write(struct intr_frame *f)
 {
-  printf("write\n");
+	printf("write\n");
 	int ret;
 	int size;
 	void *buffer;
@@ -165,4 +304,21 @@ void sys_write(struct intr_frame *f)
 	// }
 
 	return ret;
+}
+
+
+static void syscall_handler (struct intr_frame *f UNUSED) 
+{
+  	printf ("system call!\n");
+    // esp: pointer of the next-command, so syscall_number is the current command
+	// get and check syscall_number
+	int syscall_number = * (int *)f->esp;
+	printf("syscall_number: %d\n", syscall_number);
+	if(syscall_number <= 0 || syscall_number >= MAX_SYSCALL){
+		printf("syscall_number error\n");
+		thread_exit();
+	}
+	// operate syscall
+	syscalls[syscall_number](f);
+	thread_exit();
 }
